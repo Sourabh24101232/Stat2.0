4ba96fa622db209c3bf5dc4a768ca2d60461879f
Sourabh Kumar <sourabhkumar920652@gmail.com>
2026-05-30 09:17:54 +0530

    Day: 5 , created owner components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import Navbar from './components/Navbar'
-import {Route, Routes, useLocation } from 'react-router-dom'
+import { Route, Routes, useLocation } from 'react-router-dom'
 
 //imports of default export function
 import Home from './pages/Home'
@@ -9,23 +9,45 @@ import Cars from './pages/Cars'
 import MyBookings from './pages/MyBookings'
 import Footer from './components/Footer'
 
+import Layout from './pages/owner/Layout'
+import Dashboard from './pages/owner/Dashboard'
+import AddCar from './pages/owner/AddCar'
+import ManageCars from './pages/owner/ManageCars'
+import ManageBookings from './pages/owner/ManageBookings'
+
 const App = () => {
 
-  const [showogin, setShowLogin] = useState(false)
+  const [showLogin, setShowLogin] = useState(false)
   const isOwnerPath = useLocation().pathname.startsWith('/owner')
 
   return (
     <>
+
+      {/* Conditional Navbar */}
       {!isOwnerPath && <Navbar setShowLogin={setShowLogin} />}
 
       <Routes>
-        <Route path='/' element={<Home/>}/>
-        <Route path='/car-details/:id' element={<CarDetails/>}/> 
-        <Route path='/cars' element={<Cars/>}/>
-        <Route path='/my-bookings' element={<MyBookings/>}/>
+
+        <Route path='/' element={<Home />} />
+        <Route path='/cars' element={<Cars />} />
+        <Route path='/my-bookings' element={<MyBookings />} />
+        {/* Dynamic route */}
+        <Route path='/car-details/:id' element={<CarDetails />} />
+
+
+        {/* Nested Routes for owner */}
+        <Route path='/owner' element={<Layout />}>
+           {/* index means default route */}
+          <Route index element={<Dashboard />} />
+          <Route path='add-car' element={<AddCar />} />
+          <Route path='manage-cars' element={<ManageCars />} />
+          <Route path='manage-bookings' element={<ManageBookings />} />
+        </Route>
+
       </Routes>
 
-      {!isOwnerPath && <Footer/>}
+      {/* Conditional Footer */}
+      {!isOwnerPath && <Footer />}
     </>
   )
 }
